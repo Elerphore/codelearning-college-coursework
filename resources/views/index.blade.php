33914bde1b2282d45f6f3afb9fe4ebb3830262c9
Eremey Malinovsky <Elerphore@gmail.com>
2022-11-18 14:41:41 +0500

    *

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Theme Selection</title>
+</head>
+<body>
+    <div>
+        @foreach ($topics as $topic)
+            <p>
+                Тема: {{ $topic->name }}
+            </p>
+        @endforeach
+    </div>
+</body>
+</html>
